c0ea754ebd6eb76e7443085655007512b4dbd106
adugbo victory <adugbovictory@gmail.com>
2023-09-24 08:17:42 +0100

    bug fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
           >
             <NavLink to="/properties">Properties</NavLink>
 
-            <a href="mailto:zainkeepscode@gmail.com">Contact</a>
+            <a href="mailto:adugbovictory@gmail.com">Contact</a>
 
             {/* add property */}
             <div onClick={handleAddPropertyClick}>Add Property</div>
