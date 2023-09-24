b7be731b8b56429e38e57124f7eeebd7c61c0f74
adugbo victory <adugbovictory@gmail.com>
2023-09-24 07:54:48 +0100

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Suspense, useState } from "react";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
@@ -14,6 +15,8 @@ import UserDetailContext from "./context/UserDetailContext";
 import Bookings from "./pages/Bookings/Bookings";
 import Favourites from "./pages/Favourites/Favourites";
 
+import { MantineProvider } from "@mantine/core"; // Import MantineProvider
+
 function App() {
   const queryClient = new QueryClient();
 
@@ -24,27 +27,31 @@ function App() {
   });
 
   return (
-    <UserDetailContext.Provider value={{ userDetails, setUserDetails }}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route element={<Layout />}>
-                <Route path="/" element={<Website />} />
-                <Route path="/properties">
-                  <Route index element={<Properties />} />
-                  <Route path=":propertyId" element={<Property />} />
+    <MantineProvider>
+      {" "}
+      {/* Wrap your entire application with MantineProvider */}
+      <UserDetailContext.Provider value={{ userDetails, setUserDetails }}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route element={<Layout />}>
+                  <Route path="/" element={<Website />} />
+                  <Route path="/properties">
+                    <Route index element={<Properties />} />
+                    <Route path=":propertyId" element={<Property />} />
+                  </Route>
+                  <Route path="/bookings" element={<Bookings />} />
+                  <Route path="/favourites" element={<Favourites />} />
                 </Route>
-                <Route path="/bookings" element={<Bookings />} />
-                <Route path="/favourites" element={<Favourites />} />
-              </Route>
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
-        <ToastContainer />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </UserDetailContext.Provider>
+              </Routes>
+            </Suspense>
+          </BrowserRouter>
+          <ToastContainer />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </UserDetailContext.Provider>
+    </MantineProvider>
   );
 }
 
